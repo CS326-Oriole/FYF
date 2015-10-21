43ac6f68b1481b6d05e951b8d16657b83a874b59
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-10-21 17:09:05 -0400

    About page scaffolding

--- server.js
+++ server.js
@@ -1,49 +1,51 @@
-var express = require('express');
+var express    = require('express');
 var handlebars = require('express-handlebars');
-
 
 var app = express();
 
-app.set('port',process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-
-// setup handlebars 
+// setup handlebars
 var view = handlebars.create({ defaultLayout: 'main' });
 app.engine('handlebars', view.engine);
 app.set('view engine', 'handlebars');
 
+// setup static serving
+app.use(express.static(__dirname + '/public'));
 
-//call to home handlebars to show homepage
 
-app.get('/',(req, res) =>{
+///// Routes Below ///////////////////////////////////////////////////
 
-	res.render('home');
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.get('/team',(req,res)=>{
-	res.render('team');
+// Dynamic About View
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    //fortune: fortune.get(),
+    // uncomment to invoke page specific test.
+    // pageTestScript: '/qa/tests-about.js'
+  });
 });
 
-//Error routes
+//// Error Routes
 
 app.use((req, res) => {
-        res.status(404);
-	res.render('404');
+  res.status(404);
+  res.render('404');
 });
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
 
 //// Application startup
 
 app.listen(app.get('port'), () => {
-	console.log('Express started on http://localhost:' +
-	app.get('port') + '; press Ctrl-C to terminate');
-																						
+  console.log('Express started on http://localhost:' +
+              app.get('port') + '; press Ctrl-C to terminate');
 });
-
-
-
