85ca9b15987d899baa6e0224c005b3174bffb6e7
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-11-15 23:32:00 -0500

    Login successful with IA3 code. Need to modify user.js for project and probably add some styling to the page

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,14 +1,14 @@
 var express = require('express');
 
 // This gives us access to the user "model".
-//var model = require('../lib/user');
+var model = require('../lib/user');
 
 // This creates an express "router" that allows us to separate
 // particular routes from the main application.
 var router = express.Router();
 
 // A list of users who are online:
-//var online = require('../lib/online').online;
+var online = require('../lib/online').online;
 
 // Provides a login view
 router.get('/login', (req, res) => {
@@ -24,6 +24,73 @@ router.get('/login', (req, res) => {
     var message = req.flash('login') || '';
     res.render('login', { title   : 'User Login',
                           message : message });
+  }
+});
+
+// Performs **basic** user authentication.
+router.post('/auth', (req, res) => {
+  // Grab the session if the user is logged in.
+  var user = req.session.user;
+
+  console.log("AUTHENTICATION");
+
+  // Redirect to main if session and user is online:
+  if (user && online[user]) {
+    res.redirect('/user/home');    
+  }
+  else {
+    // Pull the values from the form:
+    var name = req.body.name;
+    var pass = req.body.pass;
+
+    if (!name || !pass) {
+      req.flash('login', 'did not provide the proper credentials');
+      res.redirect('/user/login');
+    }
+    else {
+      model.lookup(name, pass, function(error, user) {
+        if (error) {
+          // Pass a message to login:
+          req.flash('login', error);
+          res.redirect('/user/login');
+        }
+        else {
+          // add the user to the map of online users:
+          online[user.name] = user;
+
+          // create a session variable to represent stateful connection
+          req.session.user = user;
+
+          // Pass a message to main:
+          req.flash('main', 'authentication successful');
+          res.redirect('/user/home');
+        }
+      });
+    }
+  }
+});
+
+// Renders the main user view.
+router.get('/home', function(req, res) {
+  // Grab the user session if it exists:
+  var user = req.session.user;
+
+  console.log("HELLO" + user);
+
+  // If no session, redirect to login.
+  if (!user) {
+    req.flash('login', 'Not logged in');
+    res.redirect('/user/login');
+  }
+  else if (user && !online[user.name]) {
+    req.flash('login', 'Login Expired');
+    delete req.session.user;
+    res.redirect('/user/login')
+  }
+  else {
+    // capture the user object or create a default.
+    var message = req.flash('home') || 'Login Successful';
+    res.render('home', { });
   }
 });
 
