a916b70c2d8c8f912d35966a451d92d863c93def
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-11-24 00:25:25 -0500

    Added functionality for adding chats to the page. Cannot chat yet, creating a model for chatboxes

---
+++ public/js/chat.js
@@ -0,0 +1,7 @@
+var count = 1;
+
+$(".addChat button").click(function() {
+  $(".chatContainer").append('<div class="chatBox col-md-3" style="display: none;"> <div class="conversation"> </div> <input type = "text" name ="input" placeholder = "Type your message here!"><br> </div>');
+  count++;
+  $(".chatBox:nth-child(" + count + ")").fadeIn(500);
+});
